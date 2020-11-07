98c913db3bd6f781d844f4769256be98443dc88d
ZZKSZYJDD <51317988+ZZKSZYJDD@users.noreply.github.com>
2020-11-07 20:55:15 +0800

    Initial commit

---
+++ src/main/java/com/zut/DIUDIU/DiudiuApplication.java
@@ -0,0 +1,13 @@
+package com.zut.DIUDIU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiudiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiudiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zut/DIUDIU/DiudiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zut.DIUDIU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiudiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
